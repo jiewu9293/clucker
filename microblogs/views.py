68342a19533efb4bf7a215746e414bd4648942d1
jiewu deng <jiewu9293@gmail.com>
2026-01-23 16:28:28 +0000

    Process successful and unsuccessful log ins

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import SignUpForm, LogInForm
@@ -21,5 +22,14 @@ def feed(request):
     return render(request,'feed.html')
 
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('feed')
     form = LogInForm()
     return render(request,'log_in.html',{'form':form})
