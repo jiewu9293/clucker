c69aead7d1f18d74c7e692915a498785871831e2
jiewu deng <jiewu9293@gmail.com>
2026-01-23 10:49:11 +0000

    implement sign up POST and add unit tests for POST

--- microblogs/tests/test_sign_up_view.py
+++ microblogs/tests/test_sign_up_view.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.hashers import check_password
 from django.test import TestCase
 
 from microblogs.forms import SignUpForm
 from django.urls import reverse
+from microblogs.models import User
 
 class SignUpViewTestCase(TestCase):
     def setUp(self):
@@ -28,14 +30,28 @@ class SignUpViewTestCase(TestCase):
     def test_unsuccessful_sign_up(self):
         url = reverse('sign_up')
         self.form_input['username']='BAD_USERNAME'
+        before_count = User.objects.count()
         response = self.client.post(self.url,self.form_input)
+        after_count = User.objects.count()
+        self.assertEqual(after_count,before_count)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response,'sign_up.html')
         form = response.context['form']
         self.assertTrue(isinstance(form,SignUpForm))
         self.assertTrue(form.is_bound)
     def test_successful_sign_up(self):
+        before_count = User.objects.count()
         response = self.client.post(self.url, self.form_input,follow=True)
+        after_count = User.objects.count()
+        self.assertEqual(after_count,before_count+1)
         response_url = reverse('feed')
         self.assertRedirects(response,response_url,status_code=302,target_status_code=200)
         self.assertTemplateUsed(response, 'feed.html')
+        user = User.objects.get(username='@johndoe')
+        self.assertEqual(user.first_name,'John')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.email, 'johndoe@example.org')
+        self.assertEqual(user.bio, 'my bio')
+        is_password_correct = check_password('Password123',user.password)
+        self.assertTrue(is_password_correct)
+
--- microblogs/views.py
+++ microblogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import SignUpForm
+from .models import User
 
 # Create your views here.
 def home(request):
@@ -9,7 +10,16 @@ def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+            User.objects.create_user(
+                form.cleaned_data.get('username'),
+                email = form.cleaned_data.get('email'),
+                first_name = form.cleaned_data.get('first_name'),
+                last_name = form.cleaned_data.get('last_name'),
+                bio = form.cleaned_data.get('bio'),
+                password = form.cleaned_data.get('new_password')
+            )
             return redirect('feed')
+
     else:
         form = SignUpForm()
     return render(request,'sign_up.html',{'form':form})
