d84d4bff98148c942c06c1b01e4c28f86220a157
jiewu deng <jiewu9293@gmail.com>
2026-01-25 02:00:58 +0000

    add gravatar to requirement.txt and import it in models.py, define garvatar and mini gravatar and full name method in user model

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import RegexValidator, MaxLengthValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from libgravatar import Gravatar
 # Create your models here.
 
 class User(AbstractUser):
@@ -13,6 +14,19 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50,blank=False)
     email = models.EmailField(blank=False,unique=True)
     bio = models.TextField(blank=True,validators=[MaxLengthValidator(520)])
+    
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
+
+    def mini_gravatar(self):
+        """Return a URL to a miniature version of the user's gravatar."""
+        return self.gravatar(size=60)
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
